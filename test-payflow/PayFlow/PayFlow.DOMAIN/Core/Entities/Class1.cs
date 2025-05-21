ec4bd04528aa4d08769b12861383d16b539407ca
willtl2000 <wilber.ticona@outlook.com>
2025-05-21 07:03:17 -0500

    TEst borrar archivo

--- test-payflow/PayFlow/PayFlow.DOMAIN/Core/Entities/Class1.cs
+++ test-payflow/PayFlow/PayFlow.DOMAIN/Core/Entities/Class1.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace PayFlow.DOMAIN.Core.Entities
+﻿namespace PayFlow.DOMAIN.Core.Entities
 {
     internal class Class1
     {
